b3387ec96ca8faa264dafc3407e274b73dcfd8b8
Isamzvelasco <isabella.munoz.velasco@correounivalle.edu.co>
2026-06-29 23:18:59 -0500

    modificacion 1

--- Maximos y Minimos - P2.py
+++ Maximos y Minimos - P2.py
@@ -2,94 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import lagrange
 
+arreglo = [10, 95, 110, 90, 40]
 
-# Creamos el vector del ejemplo de clase
-x = np.linspace(0, 2*np.pi, 17)
+x = np.arange(len(arreglo))
+y = np.array(arreglo)
 
-y = []
-for i in x:
-    y.append(np.sin(i))
+polinomio = lagrange(x, y)
+print("Grado del polinomio:", polinomio.order)
 
-y = np.array(y)
+pos_max = np.argmax(y)
 
+x_vecino = x[pos_max-1:pos_max+2]
+y_vecino = y[pos_max-1:pos_max+2]
 
-# Polinomio interpolante de Lagrange
-Polinomio_Lagrange = lagrange(x, y)
-print("Grado del polinomio de Lagrange:", Polinomio_Lagrange.order)
+parabola = lagrange(x_vecino, y_vecino)
 
+x_parabola = np.linspace(x_vecino[0], x_vecino[-1], 1000)
+y_parabola = parabola(x_parabola)
 
-# Máximo usando el punto máximo y sus vecinos
-indice_max = np.argmax(y)
+pos_parabola = np.argmax(y_parabola)
 
-x_parabola = x[indice_max-1:indice_max+2]
-y_parabola = y[indice_max-1:indice_max+2]
+x_max_parabola = x_parabola[pos_parabola]
+y_max_parabola = y_parabola[pos_parabola]
 
-Parabola = lagrange(x_parabola, y_parabola)
+x_grafica = np.linspace(x[0], x[-1], 1000)
+y_grafica = polinomio(x_grafica)
 
-x_fino_parabola = np.linspace(x_parabola[0], x_parabola[-1], 1000)
-y_fino_parabola = Parabola(x_fino_parabola)
+pos_polinomio = np.argmax(y_grafica)
 
-indice_parabola = np.argmax(y_fino_parabola)
+x_max_polinomio = x_grafica[pos_polinomio]
+y_max_polinomio = y_grafica[pos_polinomio]
 
-x_max_parabola = x_fino_parabola[indice_parabola]
-y_max_parabola = y_fino_parabola[indice_parabola]
+print("Resultado usando el polinomio de Lagrange:")
+print("x =", x_max_polinomio)
+print("y =", y_max_polinomio)
 
-
-
-# 4. Derivada extrapolada del polinomio interpolante
-h = 0.01
-
-def F(x):
-    return Polinomio_Lagrange(x)
-
-def F_derivada_extrapolada(x, h):
-    cd_half = (F(x + h/4) - F(x - h/4)) / (h/2)
-    cd_full = (F(x + h/2) - F(x - h/2)) / h
-    return (4*cd_half - cd_full) / 3
-
-
-# 5. Método de punto fijo usando la derivada
-def punto_fijo(x_inicial, iteraciones):
-    x_actual = x_inicial
-
-    for i in range(iteraciones):
-        x_actual = x_actual + F_derivada_extrapolada(x_actual, h)
-
-    return x_actual
-
-x_max_punto_fijo = punto_fijo(0, 1000)
-y_max_punto_fijo = F(x_max_punto_fijo)
-
-
-
-# 6. Resultados
-print("Resultado usando solo 3 puntos y el vector generador:")
+print("\nResultado usando parábola con tres puntos:")
 print("x =", x_max_parabola)
 print("y =", y_max_parabola)
 
-print("\nResultado usando punto fijo con la derivada:")
-print("x =", x_max_punto_fijo)
-print("y =", y_max_punto_fijo)
+print("\nDiferencia entre ambos métodos:")
+print("Diferencia en x =", abs(x_max_polinomio - x_max_parabola))
+print("Diferencia en y =", abs(y_max_polinomio - y_max_parabola))
 
-print("\nDiferencia entre ambos resultados:")
-print("Diferencia en x =", abs(x_max_parabola - x_max_punto_fijo))
-print("Diferencia en y =", abs(y_max_parabola - y_max_punto_fijo))
-
-
-# 7. Gráfica
-x_fino = np.linspace(0, 2*np.pi, 1000)
-y_fino = F(x_fino)
-
-plt.plot(x_fino, y_fino, 'b', label='Polinomio de Lagrange')
-plt.plot(x_fino_parabola, y_fino_parabola, 'r--', label='Parábola con tres puntos')
+plt.plot(x_grafica, y_grafica, 'b', label='Polinomio de Lagrange')
+plt.plot(x_parabola, y_parabola, 'r--', label='Parábola con tres puntos')
 
 plt.scatter(x, y, color='black', label='Puntos originales')
+plt.scatter(x_max_polinomio, y_max_polinomio, color='blue', marker='o', label='Máximo polinomio')
 plt.scatter(x_max_parabola, y_max_parabola, color='red', marker='x', label='Máximo parábola')
-plt.scatter(x_max_punto_fijo, y_max_punto_fijo, color='blue', marker='o', label='Máximo punto fijo')
 
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Comparación del máximo por parábola y punto fijo')
+plt.title('Comparación del máximo del polinomio y la parábola')
 plt.legend()
 plt.grid()
 plt.show()
